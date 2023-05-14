563d9fd672df52b6a9842c3b6de6a58242ec28b6
Samuil Prytchyn <79350951+000004-dj@users.noreply.github.com>
2023-05-14 09:33:55 +0200

    fix: nav3

--- src/Components/Header/NavBar/NavBar.tsx
+++ src/Components/Header/NavBar/NavBar.tsx
@@ -7,6 +7,7 @@ export const NavBar = () => {
             <a href={"#Home"}>Home</a>
             <a href={"#Skills"}>Skills</a>
             <a href={"#Portfolio"}>Portfolio</a>
+
             <a href={"#Contact"}>Contact</a>
         </div>
     )
